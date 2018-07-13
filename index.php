d5ee85230f68ac43ab062299e8e9932422c03f35
Flávio Petréli <flaviojpetreli@gmail.com>
2018-07-13 00:05:57 -0300

    deixei apenas os olas

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 
-<<<<<<< HEAD
+
 echo " Olá Mundo456!";
 
-echo " Olá udemy ";
-=======
-echo " Olá Mundo123!";
-
-echo " olá hcode ";
->>>>>>> cb5535e2ce47b3807b57cdfaf4cecf6bfb8a90cb
+echo " Olá udemy e Hcode ";
 
 ?>
